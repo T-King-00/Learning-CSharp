e0348bee3b915fe81f79cc483a6791d97c8c4b83
Tony Riad <92161477+T-King-00@users.noreply.github.com>
2025-11-28 20:29:35 +0100

    -added flightstatus practice example FlightExample.cs on events and delegates

---
+++ Delegates-WEEK1/FlightExample.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace LearnCSharp.Delegates;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        Flight flight = new Flight();
+        
+      
+        //as flight passenger 
+        passenger pass1 = new passenger("mike");
+        //we add the method that gonna subscribe to the event.
+        flight.OnFlightStatusChanged +=  pass1.subscirbePassenger;
+
+        flight.UpdateStatus("boarding");
+        flight.UpdateStatus("delayed boarding");
+    }
+
+   
+    
+}
+
+class passenger
+{
+    private string name;
+    private Flight flightbooked;
+
+    public passenger(string name)
+    {
+        this.name = name;
+    }
+    public void subscirbePassenger(string flightNumber, string status)
+    {
+        Console.WriteLine($"Passenger {this.name} notified: Flight {flightNumber} is now '{status}'.");
+        
+    }
+}
+
+//publisher 
+public class Flight
+{
+    private string FlightStatus = "ontime";
+    private DateTime FlightDate = DateTime.Parse("28/11/2025");
+    private string FlightNumber = "axy145";
+    
+    //defines how subscriber will recieve the updates . 
+    public delegate void FlightStatusHandler(string flightNumber, string status);
+    public event FlightStatusHandler OnFlightStatusChanged;
+    
+    
+    public Flight()
+    {
+        
+    }
+
+    public void UpdateStatus(string newStatus)
+    {       
+        Console.WriteLine($"[Flight] {FlightNumber} status changed to: {newStatus}");
+        
+        //if there are subscribers , do so 
+
+        OnFlightStatusChanged?.Invoke(FlightNumber, newStatus);
+
+        
+    }
+}
---
+++ TPL-WEEK3/TPLEx3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp.TPL_WEEK3
+{
+    internal class TPLEx3
+    {
+
+
+        public static async Task Main0(string[] args)
+        {
+            var task = Task.Run(() => throw new CustomException("this is custom exception"));
+            try
+            {
+                task.Wait();
+            }
+            catch (AggregateException ex)
+            {
+               /* ex.Handle(e =>
+                {
+                Console.WriteLine(ex.Message);
+                if (e is CustomException)
+                {
+                    Console.WriteLine("Handled custom exception: " + e.Message);
+                    return true; // Indicate that the exception was handled
+                }
+                return false; // Indicate that the exception was not handled*/
+
+                foreach (var x in ex.InnerExceptions)
+                {
+                    // Handle the custom exception.
+                    if (x is CustomException)
+                    {
+                        Console.WriteLine(x.Message);
+                    }
+                    // Rethrow any other exception.
+                    else
+                    {
+                        throw x;
+                    }
+                };
+               
+        
+            }
+
+        }
+    }
+    public class CustomException : Exception
+    {
+        public CustomException(string message) : base(message) { }
+    }
+}
